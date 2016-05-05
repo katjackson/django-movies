b715f7791c5112bdc09ff0923b13eb5acba233a4
Kathryn Jackson <kathryn.jackson.nc@gmail.com>
2016-05-04 23:17:11 -0400

    All data loaded. Admin page up

--- movieratings/ratingbase/admin.py
+++ movieratings/ratingbase/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Rating, Movie, Rater
 
-# Register your models here.
+admin.site.register(Rater)
+admin.site.register(Movie)
+admin.site.register(Rating)
