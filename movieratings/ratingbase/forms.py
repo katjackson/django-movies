d0f163064f2f05a21943191c4b9be3c56ef924ce
Kathryn Jackson <kathryn.jackson.nc@gmail.com>
2016-05-07 20:49:01 -0400

    Added base.html

--- movieratings/ratingbase/forms.py
+++ movieratings/ratingbase/forms.py
@@ -3,13 +3,6 @@ from django.contrib.auth.models import User
 from .models import Rater, Movie, Rating
 
 
-class UserForm(forms.ModelForm):
-    password = forms.CharField(widget=forms.PasswordInput())
-
-    class Meta:
-        model = User
-        fields = ('username', 'email', 'password')
-
 class RaterForm(forms.ModelForm):
     class Meta:
         model = Rater
