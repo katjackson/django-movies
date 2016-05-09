0d2c40d695e209c4b1c802d720bceeb67d7d2998
Kathryn Jackson <kathryn.jackson.nc@gmail.com>
2016-05-09 11:13:03 -0400

    added accounts app, ratingbase fixtures

--- movieratings/movieratings/urls.py
+++ movieratings/movieratings/urls.py
@@ -1,10 +1,9 @@
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth import views as auth_views
-import django.contrib.auth.urls
 
 urlpatterns = [
     url(r'^ratingbase/', include('ratingbase.urls')),
+    url(r'^accounts/', include('accounts.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^', include('django.contrib.auth.urls')),
 ]
